4dc5a9297aea81d466b7d7fd48be4f76d3848d1e
Janarthanan Kugathasan <jana19930918@gmail.com>
2023-02-21 13:09:56 +0800

    Enabled autoscaling on ecs

--- Terraform/ecs.tf
+++ Terraform/ecs.tf
@@ -102,3 +102,44 @@ resource "aws_ecs_service" "go-api" {
     security_groups = [aws_security_group.ecs-service.id]
   }
 }
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 4
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.fargate.name}/${aws_ecs_service.go-api.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+  role_arn           = aws_iam_role.ecs-autoscale-role.arn
+}
+
+//Autoscaling policy
+resource "aws_appautoscaling_policy" "ecs_target_cpu" {
+  name               = "${var.project_name}_ecs_cpu_scale"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value = 80
+  }
+  depends_on = [aws_appautoscaling_target.ecs_target]
+}
+resource "aws_appautoscaling_policy" "ecs_target_memory" {
+  name               = "${var.project_name}_ecs_memory_scale"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+    target_value = 80
+  }
+  depends_on = [aws_appautoscaling_target.ecs_target]
+}
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,6 +6,6 @@ terraform {
   backend "s3" {
     bucket = "servian-devops-tf"
     key    = "gitaction"
-    region  = "ap-southeast-1"
+    region = "ap-southeast-1"
   }
 }
