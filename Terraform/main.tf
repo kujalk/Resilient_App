0cf33a36802d3ba4932c80d94ee637ef08a76cd7
Janarthanan Kugathasan <jana19930918@gmail.com>
2023-02-21 12:06:08 +0800

    updated remote state bucket

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-# terraform {
-#   backend "s3" {
-#     bucket = "gitaction-go-api-sqs"
-#     key    = "gitaction"
-#     region  = "ap-southeast-1"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket = "servian-devops-tf"
+    key    = "gitaction"
+    region  = "ap-southeast-1"
+  }
+}
